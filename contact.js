fa4e0629e957ade4e0fb3ace8a3e3a53f1c2e34d
jwei26 <108205537+jwei26@users.noreply.github.com>
2023-06-19 20:22:12 -0700

    update all

--- contact.js
+++ contact.js
@@ -14,7 +14,7 @@ const app = Vue.createApp({
                 email: this.email,
                 message: this.message
             };
-            fetch('http://fakeapi.com/submit', {
+            fetch('https://jsonplaceholder.typicode.com/posts', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -33,3 +33,37 @@ const app = Vue.createApp({
 })
 
 app.mount('#app')
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+    var isValid = true;
+
+    if (!name.match(/^[a-zA-Z]{2,}$/)) {
+        document.getElementById('nameError').innerText = 'Name must be at least 2 characters long and contain only alphabets.';
+        isValid = false;
+    } else {
+        document.getElementById('nameError').innerText = '';
+    }
+
+    if (!email.match(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)) {
+        document.getElementById('emailError').innerText = 'Email is invalid.';
+        isValid = false;
+    } else {
+        document.getElementById('emailError').innerText = '';
+    }
+
+    if (message.trim().length < 1) {
+        document.getElementById('messageError').innerText = 'Message cannot be empty.';
+        isValid = false;
+    } else {
+        document.getElementById('messageError').innerText = '';
+    }
+
+    if (!isValid) {
+        event.preventDefault();
+    }
+});
+
+
